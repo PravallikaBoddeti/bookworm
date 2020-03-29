7e102c802ccb6071bc1bd68c64fcbf3f47e1a17e
Pravallika <05b.pravallika@gmail.com>
2020-03-29 21:51:54 +0530

    first commit

--- bookworm-react/src/components/pages/LoginPage.js
+++ bookworm-react/src/components/pages/LoginPage.js
@@ -1,1 +1,35 @@
-import React from 'react';import LoginForm from '../forms/LoginForm';import PropTypes from 'prop-types';import { connect } from 'react-redux';import {login} from '../../actions/auth';import { Link } from 'react-router-dom';class LoginPage extends React.Component{	submit = data => {		return this.props.login(data).then(() => this.props.history.push("/dashboard"));	}	render(){		return(			<div>				<h1> LoginPage </h1>				<LoginForm submit={this.submit}/>				<Link to = "/forgot_password">Forgot Password</Link>			</div>		);	}}LoginPage.propTypes = {	history: PropTypes.shape({		push: PropTypes.func.isRequired	}).isRequired,	login: PropTypes.func.isRequired}export default connect(null, {login})(LoginPage);
+import React from "react";
+import LoginForm from "../forms/LoginForm";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { login } from "../../actions/auth";
+import { Link } from "react-router-dom";
+
+class LoginPage extends React.Component {
+  submit = data => {
+    return this.props
+      .login(data)
+      .then(() => this.props.history.push("/dashboard"));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1> LoginPage </h1>
+
+        <LoginForm submit={this.submit} />
+
+        <Link to="/forgot_password">Forgot Password</Link>
+      </div>
+    );
+  }
+}
+
+LoginPage.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }).isRequired,
+  login: PropTypes.func.isRequired
+};
+
+export default connect(null, { login })(LoginPage);
